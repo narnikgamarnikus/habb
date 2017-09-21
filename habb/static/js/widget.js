c8e04f090e249189b256f11c32a526ed8b636a82
Narnik Gamarnik <narnikgamarnikus@gmail.com>
2017-09-21 18:04:29 +0400

    changed widget js

--- habb/static/js/widget.js
+++ habb/static/js/widget.js
@@ -60,9 +60,11 @@ function main() {
 
         // We can use jQuery 1.4.2 here
         jQuery('body').append(
-          '<a href="#popup1" style="position: fixed; top: 12.5em; right: 0;">' +
+          '<div style="position: fixed; top: 12.5em; right: 0;">' +
+          '<a href="#popup1">' +
           '<img style="width: 5em; height: 5em" id="surprise" src="https://it.utah.edu/_images/taylor/gift.png">' +
-          '</a>'
+          '</a>' +
+          '</div>'
           )
         jQuery('body').append(
           '<div id="popup1" class="overlay">' +
