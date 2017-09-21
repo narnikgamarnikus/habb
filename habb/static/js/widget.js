05988e9b3b522aade9343c3be157f46617c40f9d
Narnik Gamarnik <narnikgamarnikus@gmail.com>
2017-09-21 16:32:14 +0400

    changed widget js

--- habb/static/js/widget.js
+++ habb/static/js/widget.js
@@ -25,7 +25,7 @@ if (window.jQuery === undefined || window.jQuery.fn.jquery !== '3.2.1') {
     jQuery = window.jQuery;
     main();
 }
-
+  
 /******** Called once jQuery has loaded ******/
 function scriptLoadHandler() {
     // Restore $ and window.jQuery to their previous values and store the
@@ -40,6 +40,12 @@ function scriptLoadHandler() {
 function main() { 
     jQuery(document).ready(function($) {
 
+        var popup_styles document.createElement('link');
+        popup_styles.setAttribute("rel", "stylesheet");
+        popup_styles.setAttribute("href", "https://stagingserver.xyz/static/css/popup.css");
+
+        (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(popup_styles);
+
         jQuery.fn.shake = function (interval, distance, times) {
             interval = typeof interval == "undefined" ? 100 : interval;
             distance = typeof distance == "undefined" ? 10 : distance;
@@ -53,9 +59,23 @@ function main() {
         }
 
         // We can use jQuery 1.4.2 here
-        jQuery('body').append('<img style="position: fixed; top: 12.5em; right: 0; width: 5em; height: 5em" id="surprise" src="https://it.utah.edu/_images/taylor/gift.png">')
+        jQuery('body').append(
+          '<a href="#popup1">' +
+          '<img style="position: fixed; top: 12.5em; right: 0; width: 5em; height: 5em" id="surprise" src="https://it.utah.edu/_images/taylor/gift.png">' +
+          '</a>'
+          )
+        jQuery('body').append(
+          '<div id="popup1" class="overlay">' +
+          '<div class="popup">' +
+          '<h2>Here i am</h2>' +
+          '<a class="close" href="#">&times;</a>' +
+          '<div class="content">Thank to pop me out of that button, but now im done so you can close this window.</div>' +
+          '</div>' +
+          '</div>'
+          )
         surprise = jQuery('#surprise');
         surprise.shake(100,10,300);
+
         alert('ready');
         console.log('fuck that shit!')
     });
