b5672d8337be850ec5bf768fa330c071eaefb60d
Narnik Gamarnik <narnikgamarnikus@gmail.com>
2017-09-25 14:20:46 +0400

    delete bootstrap styles

--- habb/templates/widgets/widget_detail.js
+++ habb/templates/widgets/widget_detail.js
@@ -109,7 +109,7 @@ function main() {
         // We can use jQuery 1.4.2 here
         
         jQuery('body').append(
-          '<a href="popup1">' +
+          '<a href="#popup1">' +
           '<div style="position: fixed; top: 45%; right: 0;">' +
           '<img style="width: 5em; height: 5em" id="surprise" src="https://it.utah.edu/_images/taylor/gift.png">' +
           '</div>' +
