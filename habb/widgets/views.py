a208aad102eebf1f374dbf90a4fb89009a10689d
Narnik Gamarnik <narnikgamarnikus@gmail.com>
2017-09-25 23:44:36 +0400

    added test data for AJAX sending

--- habb/widgets/views.py
+++ habb/widgets/views.py
@@ -40,7 +40,7 @@ class WidgetDetailView(LoginRequiredMixin, DetailView):
 
     def get(self, request, *args, **kwargs):
         self.object = self.get_object()
-        self.object.views += 1
+        self.object.viewed += 1
         self.object.save()
         context = self.get_context_data(object=self.object)
         return self.render_to_response(context)
