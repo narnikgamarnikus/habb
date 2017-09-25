1bd35d955cd144a33f1e42e000cc5ba33f9acfdd
Narnik Gamarnik <narnikgamarnikus@gmail.com>
2017-09-26 00:27:33 +0400

    added test data for AJAX sending

--- habb/templates/widgets/widget_detail.js
+++ habb/templates/widgets/widget_detail.js
@@ -41,7 +41,6 @@ if (window.jQuery === undefined || window.jQuery.fn.jquery !== '3.2.1') {
     // Try to find the head, otherwise default to the documentElement
     (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(script_tag);
     //(document.getElementsByTagName("head")[0] || document.documentElement).appendChild(popper_script);
-    (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(bootstrap_script);
     //(document.getElementsByTagName("head")[0] || document.documentElement).appendChild(bootstrapmd_script);
 
 } else {
@@ -68,6 +67,7 @@ function scriptLoadHandler() {
 function main() { 
     jQuery(document).ready(function($) {
         localStorage.removeItem("surpriseClicked");
+        (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(bootstrap_script);
 
 
         var popup_styles = document.createElement('link');
