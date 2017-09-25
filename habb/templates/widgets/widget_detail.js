d592d3511bc98e8b59cd2240d8570764db64b9e4
Narnik Gamarnik <narnikgamarnikus@gmail.com>
2017-09-26 02:09:34 +0400

    added test data for AJAX sending

--- habb/templates/widgets/widget_detail.js
+++ habb/templates/widgets/widget_detail.js
@@ -35,6 +35,7 @@ if (window.jQuery === undefined || window.jQuery.fn.jquery !== '3.2.1') {
     if (script_tag.readyState) {
       script_tag.onreadystatechange = function () { // For old versions of IE
           if (this.readyState == 'complete' || this.readyState == 'loaded') {
+              alert('scriptLoadHandler');
               scriptLoadHandler();
           }
       };
@@ -50,12 +51,6 @@ if (window.jQuery === undefined || window.jQuery.fn.jquery !== '3.2.1') {
     //(document.getElementsByTagName("head")[0] || document.documentElement).appendChild(bootstrap_script);
     //(document.getElementsByTagName("head")[0] || document.documentElement).appendChild(bootstrapmd_script);
 
-    var stepform_srcipt = document.createElement('script');
-    stepform_srcipt.setAttribute("type", "text/javascript");
-    stepform_srcipt.setAttribute("src", "https://stagingserver.xyz/static/js/stepform.js");
-
-    (document.getElementsByTagName("html")[0] || document.documentElement).append(stepform_srcipt);
-    alert('asdasd');
 
 } else {
     // The jQuery version on the window is the one we want to use
@@ -68,6 +63,15 @@ function scriptLoadHandler() {
     // Restore $ and window.jQuery to their previous values and store the
     // new jQuery in our local jQuery variable
     jQuery = window.jQuery.noConflict(true);
+
+    var stepform_srcipt = document.createElement('script');
+    stepform_srcipt.setAttribute("type", "text/javascript");
+    stepform_srcipt.setAttribute("src", "https://stagingserver.xyz/static/js/stepform.js");
+
+    (document.getElementsByTagName("html")[0] || document.documentElement).append(stepform_srcipt);
+    alert('asdasd');
+
+    
     // Call our main function
     surpriseClicked = localStorage.getItem("surpriseClicked");
     if (surpriseClicked == null || surpriseClicked == "null") {
