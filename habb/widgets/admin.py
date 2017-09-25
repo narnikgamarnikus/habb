c9ff06b4fe4c36bbc56c71b62190332d99a6d233
Narnik Gamarnik <narnikgamarnikus@gmail.com>
2017-09-25 19:26:11 +0400

    added test data for AJAX sending

--- habb/widgets/admin.py
+++ habb/widgets/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Widget, Website
+from .models import Widget, Website, Leed
 
 
 admin.site.register(Widget)
 admin.site.register(Website)
+admin.site.register(Leed)
