c167a8b59c5bd23e649699d50adc9bd52b1af583
Narnik Gamarnik <narnikgamarnikus@gmail.com>
2017-10-11 00:28:07 +0400

    changed load.js

--- habb/static/js/load.js
+++ habb/static/js/load.js
@@ -6,13 +6,25 @@ window.onload = function() {
     }
 
     var iframe = document.createElement('iframe');
-    iframe.id = 'widget';
+    iframe.id = 'it-zombie-widget';
     iframe.frameBorder = 0;
     iframe.src = 'https://stagingserver.xyz/ru/widget/';
     document.body.appendChild(iframe);
 
+    var css = '#it-zombie-widget { position: fixed; top: 0; left: 0; margin: 0; padding: 0; width: 180px; height: 160px; }',
+        head = document.head || document.getElementsByTagName('head')[0],
+        style = document.createElement('style');
+
+    style.type = 'text/css';
+    if (style.styleSheet){
+        style.styleSheet.cssText = css;
+    } else {
+        style.appendChild(document.createTextNode(css));
+    }
+    head.appendChild(style);
+
     window.addEventListener('message', function (event) {
-        var widget = $('#widget');
+        var widget = $('#it-zombie-widget');
         if (event.data.token === "123456") {
             if (event.data.event === 'open') {
                 widget.css('height', '100%');
