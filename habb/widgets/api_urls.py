30b4c25f2e7500f38657e4d904c4241787aeecda
Narnik Gamarnik <narnikgamarnikus@gmail.com>
2017-10-15 21:32:11 +0400

    added token field to serializer

--- habb/widgets/api_urls.py
+++ habb/widgets/api_urls.py
@@ -3,8 +3,8 @@ from habb.widgets import views
 
 
 urlpatterns = [
-	url(r'^leeds/$', views.APILeedView.as_view()),
-	#url(r'^leeds/(?P<pk>[0-9]+)/$', views.APILeedView.as_view()),
+	url(r'^leeds/$', views.APILeedListView.as_view()),
+	url(r'^leeds/(?P<pk>[0-9]+)/$', views.APILeedDetailView.as_view()),
 	#url(r'^widgets/$', views.WidgetList.as_view()),
 	url(r'^widgets/(?P<pk>[0-9]+)/$', views.APIWidgetView.as_view())
 ]
