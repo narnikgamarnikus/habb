3305585aef15f6e2552fbc2dd38819d4eee62066
Narnik Gamarnik <narnikgamarnikus@gmail.com>
2017-10-16 00:03:10 +0400

    added schema

--- habb/widgets/api_urls.py
+++ habb/widgets/api_urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from habb.widgets import views
-
+from rest_framework.schemas import get_schema_view
+schema_view = get_schema_view(title='Pastebin API')
 
 urlpatterns = [
+    url(r'^schema/$', schema_view),
 	url(r'^leeds/$', views.APILeedListView.as_view()),
 	url(r'^leeds/(?P<pk>[0-9]+)/$', views.APILeedDetailView.as_view()),
 	#url(r'^widgets/$', views.WidgetList.as_view()),
