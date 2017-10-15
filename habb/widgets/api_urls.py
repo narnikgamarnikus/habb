0c0f5f18f4e3c61bf411b51f5dc09dae33847c08
Narnik Gamarnik <narnikgamarnikus@gmail.com>
2017-10-15 20:44:49 +0400

    delete route with leed pk arg

--- habb/widgets/api_urls.py
+++ habb/widgets/api_urls.py
@@ -4,7 +4,7 @@ from habb.widgets import views
 
 urlpatterns = [
 	url(r'^leeds/$', views.APILeedView.as_view()),
-	url(r'^leeds/(?P<pk>[0-9]+)/$', views.APILeedView.as_view()),
+	#url(r'^leeds/(?P<pk>[0-9]+)/$', views.APILeedView.as_view()),
 	#url(r'^widgets/$', views.WidgetList.as_view()),
 	url(r'^widgets/(?P<pk>[0-9]+)/$', views.APIWidgetView.as_view())
 ]
