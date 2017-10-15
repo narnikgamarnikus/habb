0e8a1ec66727158c0b9b1c59cc77f0b25defa01b
Narnik Gamarnik <narnikgamarnikus@gmail.com>
2017-10-15 21:09:13 +0400

    added token field to serializer

--- habb/widgets/serializers.py
+++ habb/widgets/serializers.py
@@ -5,7 +5,7 @@ from .models import Leed, Widget
 class LeedSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Leed
-		fields = ('id', 'widget', 'first_name', 'last_name', 'email', 'phone_number', 'token')
+		fields = ('id', 'widget', 'first_name', 'last_name', 'email', 'phone_number', 'token', 'referal')
 
 
 class WidgetSerializer(serializers.ModelSerializer):
