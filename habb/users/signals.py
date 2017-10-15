7af9ead91b148c1be9165ad9b601051f2af5c747
Narnik Gamarnik <narnikgamarnikus@gmail.com>
2017-10-16 00:24:33 +0400

    added create signal

--- habb/users/signals.py
+++ habb/users/signals.py
@@ -1,5 +1,10 @@
 from .models import User
-from django.db.models import signals
-from tastypie.models import create_api_key
+#from django.db.models import signals
+#from tastypie.models import create_api_key
 
-signals.post_save.connect(create_api_key, sender=User)
+#signals.post_save.connect(create_api_key, sender=User)
+
+@receiver(post_save, sender=User)
+def create_etc_and_btc(sender, instance, created, **kwargs):
+	if created:
+		instance.generate_token()
